15bfd041aa2b0e40e062cda23a6a61d24a44a671
AnnisaMufidahSopian <annisams.student@gmail.com>
2022-09-27 16:36:48 -1100

    assignment 3

---
+++ assignment3/src/pages/Interest.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class Interest extends React.Component {
+  render() {
+    return (
+      <section id="interest">
+        <div className="container">
+          <div className="row">
+            <div className="col mt-5 text-center">
+              <h2>Interest</h2>
+            </div>
+          </div>
+          <div className="row justify-content-center fs-5 mt-5 mb-5">
+            <div className="col-10">
+              <p>
+               I'm a lifelong learner and i have high interest in web programming, design thinking, and AI Research. And also i love to read a self-improvement books, being an event planner, and joining an organization or communnity that focused at education, indonesian culture, etc.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Interest;
